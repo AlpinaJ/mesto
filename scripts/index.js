dad47e0574bc65ecf5a6a81f8f6ad2a2fc571e7a
julia953197 <julia953197@yandex.ru>
2021-11-16 13:27:33 +0300

    Bugfix file structure1

--- scripts/index.js
+++ scripts/index.js
@@ -10,28 +10,28 @@ const card_likes = document.querySelectorAll('.card__like');
 
 console.log(editButton);
 editButton.addEventListener('click', function () {
-    popup.classList.add('popup_isOpen');
+    popup.classList.add('popup__opened');
 });
 
 popupCloseButton.addEventListener('click', function () {
-    popup.classList.remove('popup_isOpen');
+    popup.classList.remove('popup__opened');
 });
 
 function submitForm(event) {
     event.preventDefault();
     name.textContent = newName.value;
     description.textContent = newDescription.value;
-    popup.classList.remove('popup_isOpen');
+    popup.classList.remove('popup__opened');
 }
 
 form.addEventListener('submit', submitForm);
 
 for (let i = 0; i < card_likes.length; i = i + 1) {
     card_likes[i].addEventListener('click', function () {
-        if (card_likes[i].classList.contains('card__like-active')) {
-            card_likes[i].classList.remove('card__like-active');
+        if (card_likes[i].classList.contains('card__like_active')) {
+            card_likes[i].classList.remove('card__like_active');
         } else {
-            card_likes[i].classList.add('card__like-active');
+            card_likes[i].classList.add('card__like_active');
         }
     });
 }
