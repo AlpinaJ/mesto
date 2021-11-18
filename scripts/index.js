a74ea09f2fff05f315302444c6e184d38f591628
julia953197 <julia953197@yandex.ru>
2021-11-18 15:04:06 +0300

    Bugfix1 review 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,42 @@ const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__button-close');
 const editButton = document.querySelector('.profile__edit-button');
 const form = document.querySelector('.popup__form');
-const newName = form.querySelector('.popup__input-name');
-const newDescription = form.querySelector('.popup__input-description');
+const newName = form.querySelector('.popup__input_type_name');
+const newDescription = form.querySelector('.popup__input_type_description');
 const name = document.querySelector('.profile__title');
 const description = document.querySelector('.profile__subtitle');
-const card_likes = document.querySelectorAll('.card__like');
+// const card_likes = document.querySelectorAll('.card__like');
 
-console.log(editButton);
-editButton.addEventListener('click', function () {
-    popup.classList.add('popup__opened');
-});
+function openPopup(){
+    popup.classList.add('popup_opened');
+    name.textContent = newName.value;
+    description.textContent = newDescription.value;
+}
 
-popupCloseButton.addEventListener('click', function () {
-    popup.classList.remove('popup__opened');
-});
+function closePopup(){
+    popup.classList.remove('popup_opened');
+}
 
 function submitForm(event) {
     event.preventDefault();
     name.textContent = newName.value;
     description.textContent = newDescription.value;
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
+
+editButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', closePopup);
 
 form.addEventListener('submit', submitForm);
 
-for (let i = 0; i < card_likes.length; i = i + 1) {
-    card_likes[i].addEventListener('click', function () {
-        if (card_likes[i].classList.contains('card__like_active')) {
-            card_likes[i].classList.remove('card__like_active');
-        } else {
-            card_likes[i].classList.add('card__like_active');
-        }
-    });
-}
+// for (let i = 0; i < card_likes.length; i = i + 1) {
+//     card_likes[i].addEventListener('click', function () {
+//         if (card_likes[i].classList.contains('card__like_active')) {
+//             card_likes[i].classList.remove('card__like_active');
+//         } else {
+//             card_likes[i].classList.add('card__like_active');
+//         }
+//     });
+// }
 
