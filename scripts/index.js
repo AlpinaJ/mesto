579aa8e34a58fd22824c8750a4c86d2b9ce4d55f
julia953197 <julia953197@yandex.ru>
2021-12-08 20:00:49 +0300

    5 sprint added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 // Попапы
 const popupEdit = document.querySelector('.popup_type_edit');
 const popupAdd = document.querySelector('.popup_type_add');
+const popupShow = document.querySelector('.popup_type_show');
 
 // Кнопки в профиле
 const popupCloseButtons = document.querySelectorAll('.popup__button-close');
@@ -20,32 +21,36 @@ const newPlace = addForm.querySelector('.popup__input_type_place');
 const newLink = addForm.querySelector('.popup__input_type_link');
 const createButton = addForm.querySelector('.popup__button-create');
 
+// Переменные для работы с увеличинной картинкой
+const popupImage = popupShow.querySelector('.popup__image');
+const popupText = popupShow.querySelector('.popup__text');
+
 const cards = document.querySelector('.main');
 const cardTemplate = document.querySelector('template');
 const initialCards = [
     {
-        name: 'Архыз',
-        link: 'https://images.unsplash.com/photo-1536308304182-5a2e0e04bb08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXpvcmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        name: 'Красно-золотая елка',
+        link: 'https://images.unsplash.com/photo-1638836369642-8917176c47e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
-        name: 'Челябинская область',
-        link: 'https://images.unsplash.com/photo-1620998051604-95ff17ccc537?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXpvcmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        name: 'Гирлянда на елке',
+        link: 'https://images.unsplash.com/photo-1513297887119-d46091b24bfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2hyaXN0bWFzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
-        name: 'Иваново',
-        link: 'https://images.unsplash.com/photo-1546526570-8f949b0d4a8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXpvcmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        name: 'Елка с носочками',
+        link: 'https://images.unsplash.com/photo-1543589077-47d81606c1bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNocmlzdG1hc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
-        name: 'Камчатка',
-        link: 'https://images.unsplash.com/photo-1621417052569-88fbc513a50e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGF6b3Jlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+        name: 'Размытые огни',
+        link: 'https://images.unsplash.com/photo-1603561128570-224a45ebc081?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGNocmlzdG1hc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
-        name: 'Холмогорский район',
-        link: 'https://media.istockphoto.com/photos/costal-path-with-hydrangeas-sao-miguel-azores-portugal-picture-id944487332?b=1&k=20&m=944487332&s=170667a&w=0&h=gf7qveCrCdiNvTAU33Grg3MkPco-byfTgwWgl-U-dn0='
+        name: 'Котенька',
+        link: 'https://images.unsplash.com/photo-1583083527882-4bee9aba2eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNocmlzdG1hc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     },
     {
-        name: 'Байкал',
-        link: 'https://images.unsplash.com/photo-1621246308836-ea7d366c2795?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXpvcmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        name: 'Печеньки',
+        link: 'https://images.unsplash.com/photo-1576665503527-f46e04d0844d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGNocmlzdG1hc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     }
 ];
 
@@ -87,6 +92,11 @@ addButton.addEventListener('click', function () {
     openPopup(popupAdd);
 });
 
+function deleteCard(event) {
+    const card = event.target;
+    card.closest('.card').remove();
+}
+
 function createCard(card) {
     const newCard = cardTemplate.content.cloneNode(true);
     const cardImage = newCard.querySelector('.card__image');
@@ -103,6 +113,17 @@ function createCard(card) {
         like.classList.toggle('card__like_active');
     });
 
+    // Удаление карточек
+    const trashButton = newCard.querySelector('.card__thrash');
+    trashButton.addEventListener('click', deleteCard);
+
+    // Увеличение картинки
+    cardImage.addEventListener('click', function (){
+        openPopup(popupShow);
+        popupImage.src = cardImage.src;
+        popupText.textContent = cardTitle.textContent;
+    })
+
     return newCard;
 }
 
@@ -110,18 +131,18 @@ function addCard(card) {
     cards.prepend(card);
 }
 
+
 initialCards.forEach(card => addCard(createCard(card)));
 
+// Функция для добавлении карточки из попапа
 function addPopupCard(event) {
     event.preventDefault();
-    console.log(1);
     const newCard = {
         name: newPlace.value,
         link: newLink.value,
     };
 
     addCard(createCard(newCard));
-    console.log(2);
     closePopup(popupAdd);
 }
 
